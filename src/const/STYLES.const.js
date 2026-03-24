dcaf9101030d27e4dcc5d503f705ba78184a1c47
MaksimsTurs <maksims07turs@gmail.com>
2026-03-24 17:31:07 +0100

    feat(const/STYLES): add blinking

--- src/const/STYLES.const.js
+++ src/const/STYLES.const.js
@@ -1,10 +1,11 @@
 const STYLES = {
-  RESET:           "\x1b[0m",
-  BOLD:            "\x1b[1m",
-  DIM:             "\x1b[2m",
-  ITALIC:          "\x1b[3m",
-  UNDERLINE:       "\x1b[4m",
-  STRIKE_THROUGHT: "\x1b[9m",
+  RESET:            "\x1b[0m",
+  BOLD:             "\x1b[1m",
+  DIM:              "\x1b[2m",
+  ITALIC:           "\x1b[3m",
+  UNDERLINE:        "\x1b[4m",
+  BLINKING:         "\x1b[5m",
+  STRIKE_THROUGHT:  "\x1b[9m",
 };
 
 export default STYLES;
