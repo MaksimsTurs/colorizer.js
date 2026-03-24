b7e5c650afd2f720d4f841c85ddd92d315a477f1
MaksimsTurs <maksims07turs@gmail.com>
2026-03-24 17:26:57 +0100

    refactor(const/STYLES): rewrite in js

--- src/const/STYLES.const.js
+++ src/const/STYLES.const.js
@@ -5,9 +5,6 @@ const STYLES = {
   ITALIC:          "\x1b[3m",
   UNDERLINE:       "\x1b[4m",
   STRIKE_THROUGHT: "\x1b[9m",
-  
-  BG:        "\x1b[38;2;",
-  FG:        "\x1b[48;2;",
-} as const;
+};
 
 export default STYLES;
