b4a9f7079b38b1183c9dd597edd1466327871f44
MaksimsTurs <maksims07turs@gmail.com>
2026-03-25 13:05:06 +0100

    refactor: remove is-fill-context-valid utility while doc have description how library should be used

--- src/utils/is-fill-context-valid.util.ts
+++
@@ -1,9 +0,0 @@
-import STYLES from "../const/STYLES.const.ts";
-
-export default function isFillContextValid(something?: string): boolean {
-  return(
-    !!(something &&
-      (something === STYLES.BG ||
-       something === STYLES.FG))
-  );
-};
